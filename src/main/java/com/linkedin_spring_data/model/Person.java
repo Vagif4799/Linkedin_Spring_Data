459efe7565ad51e263ef3e9ce979c890533dbfe9
vagif4799 <quliyev_vaqif@icloud.com>
2020-02-11 10:23:00 +0400

    -- Person @Embeddable added

---
+++ src/main/java/com/linkedin_spring_data/model/Person.java
@@ -0,0 +1,45 @@
+package com.linkedin_spring_data.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Person {
+
+    @Column
+    private String firstName;
+
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/linkedin_spring_data/model/Student.java
+++ src/main/java/com/linkedin_spring_data/model/Student.java
@@ -13,14 +13,17 @@ public class Student {
     @Column(name = "student_id")
     private Integer id;
 
-    @Column(name = "student_name", nullable = false)
-    private String name;
+//    @Column(name = "student_name", nullable = false)
+//    private String name;
 
     @Column(name = "student_fulltime", nullable = false)
     private boolean fullTime;
 
     @Column(name = "student_age")
     private Integer age;
+
+    @Embedded
+    private Person attendee;
 
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
@@ -30,12 +33,13 @@ public class Student {
     )
     private List<Course> courses = new ArrayList<>();
 
-    public Student(String name, boolean fullTime, Integer age) {
-        this.name = name;
+
+    public Student(boolean fullTime, Integer age, Person attendee, List<Course> courses) {
         this.fullTime = fullTime;
         this.age = age;
+        this.attendee = attendee;
+        this.courses = courses;
     }
-
 
     public Integer getId() {
         return id;
@@ -45,12 +49,20 @@ public class Student {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public Person getAttendee() {
+        return attendee;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setAttendee(Person attendee) {
+        this.attendee = attendee;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 
     public boolean isFullTime() {
